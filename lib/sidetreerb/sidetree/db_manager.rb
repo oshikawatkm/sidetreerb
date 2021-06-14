6cda4f3e257752888b288f0e7031dcb96b8adc59
wokashi <tkm05oshikawa12@gmail.com>
2021-06-15 00:11:17 +0900

    setup db manager

---
+++ lib/sidetreerb/sidetree/db_manager.rb
@@ -0,0 +1,18 @@
+
+module Sidetreerb
+  module Sidetree
+    class DBManager
+
+
+      def en_que_create_operation(create_operation)
+        db.create()
+      end
+  
+      def load_qued_create_operations
+        
+      end
+
+
+    end
+  end
+end
