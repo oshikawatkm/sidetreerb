6508667c21069f39caa9f4d27a0f267122999e5b
wokashi <tkm05oshikawa12@gmail.com>
2021-06-19 11:36:30 +0900

    load rake task

--- lib/sidetreerb.rb
+++ lib/sidetreerb.rb
@@ -8,4 +8,14 @@ module Sidetreerb
   autoload :Cas, 'sidetreerb/cas'
   autoload :DB, 'sidetreerb/db'
   autoload :Sidetree, 'sidetreerb/sidetree'
+
+  begin
+    class Railtie < ::Rails::Railtie
+      rake_tasks do 
+        load "tasks/sidetreerb/batch_writer.rake"
+      end
+    end
+  rescue => exception
+    puts "Paketask is unavailable"
+  end
 end
