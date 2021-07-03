266776ebe4ebc6617810dbbe69e2306d33b31e3e
wokashi <tkm05oshikawa12@gmail.com>
2021-07-03 15:30:42 +0900

    add operation_manager rspec

--- spec/sidereeerb/sidetree/operation_manager_spec.rb
+++ spec/sidereeerb/sidetree/operation_manager_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe 'Sidetreerb::Sidetree::OperationManager' do
       it { expect(subject).to be eq 1 }
     end
 
+    context "#generate_new_create_operation" do
+    subject { create_operation.export_json }
+    let(:create_operation) { operation_manager.generate_new_create_operation }
+
+    it { expect(subject).to be eq 1 }
+    end
+
   end
 end
